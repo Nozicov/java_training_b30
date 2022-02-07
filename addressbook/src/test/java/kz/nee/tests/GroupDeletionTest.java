9694baa1498a5b671a11c1707243ea8b6dd6c688
Nozicov <yevgeniy.nozikov@aitupay.kz>
2022-02-07 10:03:32 +0600

    Add creatGroup in deleteGroup and modifiGroup (lesson 3.10)

--- addressbook/src/test/java/kz/nee/tests/GroupDeletionTest.java
+++ addressbook/src/test/java/kz/nee/tests/GroupDeletionTest.java
@@ -1,5 +1,6 @@
 package kz.nee.tests;
 
+import kz.nee.model.GroupData;
 import org.testng.annotations.Test;
 
 public class GroupDeletionTest extends TestBase {
@@ -7,6 +8,9 @@ public class GroupDeletionTest extends TestBase {
   @Test
   public void testGroupDeletion() {
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().creatGroup(new GroupData("Group name", "Group header", "Group footer"));
+    }
     app.getGroupHelper().selectedGroup();
     app.getGroupHelper().deleteSelectedGrops();
     app.getNavigationHelper().gotoGroupPage();
--- addressbook/src/test/java/kz/nee/tests/GroupModificationTests.java
+++ addressbook/src/test/java/kz/nee/tests/GroupModificationTests.java
@@ -8,6 +8,9 @@ public class GroupModificationTests extends TestBase {
   @Test
   public void testGroupModification(){
     app.getNavigationHelper().gotoGroupPage();
+    if (! app.getGroupHelper().isThereAGroup()){
+      app.getGroupHelper().creatGroup(new GroupData("Group name", "Group header", "Group footer"));
+    }
     app.getGroupHelper().selectedGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("Name - up", "Header - up", "Footer - up"));
