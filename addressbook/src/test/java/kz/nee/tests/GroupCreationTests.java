62475a60c8ac55c6ed3add23696bf1cf21195015
Nozicov <yevgeniy.nozikov@aitupay.kz>
2022-03-20 02:17:07 +0600

    Лекция 6.5. Загрузка тестовых данных из файла

--- addressbook/src/test/java/kz/nee/tests/GroupCreationTests.java
+++ addressbook/src/test/java/kz/nee/tests/GroupCreationTests.java
@@ -6,6 +6,7 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
+import java.io.*;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -21,17 +22,15 @@ public class GroupCreationTests extends TestBase {
   }
 
   @DataProvider
-  public Iterator<Object[]> validGroups(){
+  public Iterator<Object[]> validGroups() throws IOException {
     List<Object[]> list = new ArrayList<Object[]>();
-    list.add(new Object[]{
-            new GroupData().withName("Group name 1").withHeader("Group header 1").withFooter("Group footer 1")
-    });
-    list.add(new Object[]{
-            new GroupData().withName("Group name 2").withHeader("Group header 2").withFooter("Group footer 2")
-    });
-    list.add(new Object[]{
-            new GroupData().withName("Group name 3").withHeader("Group header 3").withFooter("Group footer 3")
-    });
+    BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/groups.csv")));
+    String line = reader.readLine();
+    while (line != null){
+      String[] split = line.split(";");
+      list.add(new Object[] {new GroupData().withName(split[0]).withHeader(split[1]).withFooter(split[2])});
+      line = reader.readLine();
+    }
     return list.iterator();
   }
 
