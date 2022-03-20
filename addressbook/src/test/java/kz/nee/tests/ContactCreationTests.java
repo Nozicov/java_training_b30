f8536735c777df546b8289fa6b6fba62874cf0bc
Nozicov <yevgeniy.nozikov@aitupay.kz>
2022-03-20 13:11:21 +0600

    Лекция 6.9. Конфигурационные файлы для описания тестовых наборов

--- addressbook/src/test/java/kz/nee/tests/ContactCreationTests.java
+++ addressbook/src/test/java/kz/nee/tests/ContactCreationTests.java
@@ -40,16 +40,13 @@ public class ContactCreationTests extends TestBase {
     try(BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.xml")))){
       String xml = "";
       String line = reader.readLine();
-
       while (line != null){
         xml += line;
         line = reader.readLine();
       }
-
       XStream xstream = new XStream();
       xstream.processAnnotations(GroupData.class);
       List<ContactData> groups = (List<ContactData>)  xstream.fromXML(xml);
-
       return groups.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
     }
   }
@@ -59,12 +56,10 @@ public class ContactCreationTests extends TestBase {
     try(BufferedReader reader = new BufferedReader(new FileReader(new File("src/test/resources/contacts.json")))){
       String json = "";
       String line = reader.readLine();
-
       while (line != null){
         json += line;
         line = reader.readLine();
       }
-
       Gson gson = new Gson();
       List<ContactData> contacts = gson.fromJson(json, new TypeToken<List<ContactData>>(){}.getType());
       return contacts.stream().map((g) -> new Object[] {g}).collect(Collectors.toList()).iterator();
