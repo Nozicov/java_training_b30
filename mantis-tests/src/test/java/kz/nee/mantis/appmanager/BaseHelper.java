e2bbd992f744bf69719c069aed274490e8bdec73
Nozicov <yevgeniy.nozikov@aitupay.kz>
2022-03-23 05:55:59 +0600

    Лекция 8.6. Получение почты на встроенный в тесты почтовый сервер

---
+++ mantis-tests/src/test/java/kz/nee/mantis/appmanager/BaseHelper.java
@@ -0,0 +1,67 @@
+package kz.nee.mantis.appmanager;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.NoAlertPresentException;
+import org.openqa.selenium.NoSuchElementException;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.support.ui.Select;
+
+import java.io.File;
+
+public class BaseHelper {
+  protected ApplicationManager app;
+  protected WebDriver wd;
+
+  public BaseHelper(ApplicationManager app) {
+    this.app = app;
+    this.wd = app.getDriver();
+  }
+
+  protected void type(By locator, String text) {
+    click(locator);
+    if (text != null) {
+      String existingText = wd.findElement(locator).getAttribute("value");
+      if (! existingText.equals(text)){
+        wd.findElement(locator).clear();
+        wd.findElement(locator).sendKeys(text);
+      }
+    }
+  }
+
+  protected void attach(By locator, File file) {
+    if (file != null) {
+        wd.findElement(locator).sendKeys(file.getAbsolutePath());
+      }
+  }
+
+  protected void click(By locator) {
+    wd.findElement(locator).click();
+  }
+
+  public boolean isAlertPresent() {
+    try {
+      wd.switchTo().alert();
+      return true;
+    } catch (NoAlertPresentException e) {
+      return false;
+    }
+  }
+
+  protected boolean isElementPresent(By location) {
+    try{
+      wd.findElement(location);
+      return true;
+    } catch (NoSuchElementException ex){
+      return false;
+    }
+  }
+
+  protected boolean selectList(By locator, String text) {
+    try {
+      new Select(wd.findElement(locator)).selectByVisibleText(text);
+      return true;
+    } catch (NoSuchElementException ex) {
+      return false;
+    }
+  }
+}
