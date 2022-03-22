c26ef4acbc5c6e7a18cea0da7777e51f6fbce85f
Nozicov <yevgeniy.nozikov@aitupay.kz>
2022-03-22 09:35:18 +0600

    Лекция 8.3. Прямое взаимодействие с сервером по протоколу HTTP

---
+++ mantis-tests/src/test/java/kz/nee/mantis/tests/LoginTests.java
@@ -0,0 +1,18 @@
+package kz.nee.mantis.tests;
+
+import kz.nee.mantis.appmanager.HttpSession;
+import org.testng.annotations.Test;
+
+import java.io.IOException;
+
+import static org.testng.Assert.assertTrue;
+
+public class LoginTests extends TestBase{
+
+  @Test
+  public void testLogin() throws IOException {
+    HttpSession session = app.newSession();
+    assertTrue(session.login("administrator", "root"));
+    assertTrue(session.isLoggedInAs("administrator"));
+  }
+}
